65fdb3aadf0395eb6238356e94eaae5d469f605b
AlexAI <zavrazin2006@mail.ru>
2026-02-04 10:10:33 +0700

    task_1

---
+++ task_1/task.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#ifdef USE_DOUBLE
+    using real_t = double;
+#else
+    using real_t = float;
+#endif
+
+int main(){
+    const int N = 10000000;
+    const real_t step = real_t(0.1);
+    int sum = 0;
+
+    vector<real_t> sin_values(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        real_t x = static_cast<real_t>(i) * step;
+        sin_values[i] = sin(x);
+    }
+
+    for (const auto& value : sin_values)
+    {
+        sum += value;
+    }
+    
+    cout << sum << endl;
+    
+    return 0;
+}
