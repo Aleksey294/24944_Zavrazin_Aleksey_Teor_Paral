c0240f420314cd49d4db2afdb7c89b75cd6ad36c
AlexAI <zavrazin2006@mail.ru>
2026-02-10 14:55:19 +0700

    update

--- task_1/task.cpp
+++ task_1/task.cpp
@@ -12,14 +12,12 @@ using namespace std;
 
 int main(){
     const int N = 10000000;
-    const real_t step = real_t(0.1);
     real_t sum = 0;
-
     vector<real_t> sin_values(N);
 
     for (int i = 0; i < N; i++)
     {
-        real_t x = static_cast<real_t>(i) * step;
+        real_t x = i * 10 * M_PI / 180; 
         sin_values[i] = sin(x);
         sum += sin_values[i];
     }
